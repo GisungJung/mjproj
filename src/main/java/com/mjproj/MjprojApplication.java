6db490c0e4cea2725bab35ea674fcabf14f99945
bizdata\Bizdata0 <gisung@bizdata.kr>
2024-11-14 14:26:27 +0900

    커밋

---
+++ src/main/java/com/mjproj/MjprojApplication.java
@@ -0,0 +1,13 @@
+package com.mjproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MjprojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mjproj/MjprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjprojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
