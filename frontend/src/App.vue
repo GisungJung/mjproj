58a90e1fdee705f4c6149129acdf8d717cacfe8b
bizdata\Bizdata0 <gisung@bizdata.kr>
2024-11-14 15:55:07 +0900

    지도 올림

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,29 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <MainMap />
+  </div>
 </template>
 
+<script>
+import MainMap from '@/components/MainMap.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainMap
+  }
+}
+
+</script>
+
 <style>
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ frontend/src/components/MainMap.vue
@@ -0,0 +1,48 @@
+<template>
+  <div ref="map" class="main-map">
+  </div>
+</template>
+
+<script>
+import OlLayerTile from 'ol/layer/Tile.js'
+import OlView from 'ol/View.js'
+import OlMap from 'ol/Map.js'
+import OSM from 'ol/source/OSM'
+import { fromLonLat } from 'ol/proj.js'
+import { defaults } from 'ol/control.js'
+
+export default {
+  name: 'MainMap',
+  data () {
+    return {
+      olMap: undefined
+    }
+  },
+  mounted () {
+    this.olMap = new OlMap({
+      target: this.$refs.map,
+      controls: defaults({
+        attribution: false,
+        zoom: false,
+        rotate: false
+      }),
+      layers: [
+        new OlLayerTile({
+          source: new OSM()
+        })
+      ],
+      view: new OlView({
+        center: fromLonLat([127.03507835, 37.48352327]), // 양재
+        zoom: 18
+      })
+    })
+  }
+}
+</script>
+
+<style scoped>
+.main-map {
+  width: 100%;
+  height: 100%;
+}
+</style>
