c0c718c29895cfde47fbcfc7d44f719fabc4c6dc
bizdata\Bizdata0 <gisung@bizdata.kr>
2024-11-14 17:27:09 +0900

    지도위 올림

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,25 @@
 <template>
   <div id="app">
     <MainMap />
+    <SideBar class="side-bar"/>
   </div>
 </template>
 
 <script>
 import MainMap from '@/components/MainMap.vue'
+import SideBar from '@/components/SideBar.vue'
 
 export default {
   name: 'App',
   components: {
-    MainMap
+    MainMap,
+    SideBar
   }
 }
 
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 #app {
   position: absolute;
@@ -24,6 +27,13 @@ export default {
   right: 0;
   bottom: 0;
   left: 0;
+
+  > .side-bar {
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+  }
 }
 
 </style>
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,17 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const { defineConfig } = require('@vue/cli-service');
 
-module.exports = {
-  outputDir: "../src/main/resources/static",  // 빌드 타겟 디렉토리
+module.exports = defineConfig({
+  transpileDependencies: true,
+  outputDir: '../src/main/resources/static', // 빌드 타겟 디렉토리
   devServer: {
     proxy: {
       '/api': {
-        // '/api' 로 들어오면 포트 8081(스프링 서버)로 보낸다
-        target: 'http://localhost:8081',
+        target: 'http://localhost:8081', // target 값 추가
         changeOrigin: true // cross origin 허용
       }
     }
   }
-};
+});
